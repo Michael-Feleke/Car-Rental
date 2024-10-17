12fa944f02e7185d3e587d2c1c20e2f3cc8e9ce6
Michael-Feleke <felekemichael999@gmail.com>
2024-10-17 12:32:39 +0300

    Refactor Logo component to use smaller image size

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -5,13 +5,7 @@ import Link from "next/link";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image
-        src={logo}
-        height="150"
-        width="150"
-        className=" border-green-400 border-solid border-2 rounded-full "
-        alt="The Go Drive logo"
-      />
+      <Image src={logo} height="90" width="90" alt="The Go Drive logo" />
     </Link>
   );
 }
