801cf47bddd4e9185ae03168e5f67d30f1f59954
Michael-Feleke <felekemichael999@gmail.com>
2024-10-17 14:36:37 +0300

    Refactor border color in Header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <header className="border-b  border-gray-700 px-8 py5">
+    <header className="border-b  border-gray-800 px-8 py5">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />
