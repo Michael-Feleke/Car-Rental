0177ab18517af211d68201087605ab6231108236
Michael-Feleke <felekemichael999@gmail.com>
2024-10-17 12:33:01 +0300

    Add route constants for home, cars, about, and account

---
+++ app/_utils/routes/index.ts
@@ -0,0 +1,8 @@
+import { IRoute } from "./types";
+
+export const routes: IRoute[] = [
+  { path: "/", label: "Home" },
+  { path: "/cars", label: "Cars" },
+  { path: "/about", label: "About" },
+  { path: "/account", label: "Your Account" },
+];
