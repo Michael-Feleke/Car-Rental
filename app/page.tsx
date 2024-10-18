e22da19cea08838662195912be95ae29477555dd
Michael-Feleke <felekemichael999@gmail.com>
2024-10-18 12:16:16 +0300

    Refactor global CSS styles in layout.tsx and add route constants for home, cars, about, and account

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import { routeConstants } from "./_utils/constants";
+import backgroundImage from "@/public/images/bg-image.jpeg";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="mt-24">
+      <Image
+        src={backgroundImage}
+        fill
+        quality={80}
+        placeholder="blur"
+        className="object-cover object-top "
+        alt="Background image of a car"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl  mb-10 tracking-tight font-normal">
+          Welcome to
+          <span className="text-red-600"> GoDrive </span>
+          Car Rental
+        </h1>
+        <Link
+          href={routeConstants.cars}
+          className="bg-red-600 px-8 py-5 text-lg font-semibold hover:bg-red-700 transition-all duration-200 ease-in-out rounded-lg"
+        >
+          View Cars
+        </Link>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
