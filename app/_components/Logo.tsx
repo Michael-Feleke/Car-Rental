e4550def54eaaea525f461844dc73c7b08d57f22
Michael-Feleke <felekemichael999@gmail.com>
2024-10-18 12:16:49 +0300

    Refactor Logo component to add rounded border and improve image quality

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -5,7 +5,14 @@ import Link from "next/link";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image src={logo} height="90" width="90" alt="The Go Drive logo" />
+      <Image
+        src={logo}
+        height="90"
+        width="90"
+        quality={100}
+        className="rounded-full"
+        alt="The Go Drive logo"
+      />
     </Link>
   );
 }
