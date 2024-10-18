dc9a5d306aaa8148ee2f889e06173e77591f3cb1
Michael-Feleke <felekemichael999@gmail.com>
2024-10-18 12:16:55 +0300

    Refactor Navigation component to improve active route highlighting

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -3,19 +3,26 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { routes } from "../_utils/routes";
+import { routeConstants } from "../_utils/constants";
 
 function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav>
-      <ul className="flex space-x-8 p-4 bg-background">
+    <nav className="z-10">
+      <ul className="flex space-x-8 p-">
         {routes.map((route) => (
           <li key={route.path}>
             <Link
               href={route.path}
               className={`text-xl ${
-                pathname === route.path ? "text-red-500" : "text-gray-300"
+                pathname === routeConstants.home &&
+                route.path === routeConstants.home
+                  ? "text-red-500"
+                  : pathname.startsWith(route.path) &&
+                    route.path !== routeConstants.home
+                  ? "text-red-500"
+                  : "text-gray-300"
               } hover:text-red-500 transition duration-200 ease-in-out`}
             >
               {route.label}
