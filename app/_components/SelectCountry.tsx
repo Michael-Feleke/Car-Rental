867a4d0e1c896f05e1ba8b67a0533756237ed0a8
Michael-Feleke <felekemichael999@gmail.com>
2024-10-18 12:17:11 +0300

    Add SelectCountry component for selecting a country from a dropdown menu

---
+++ app/_components/SelectCountry.tsx
@@ -0,0 +1,33 @@
+import { getCountries } from "../_api/getCountries";
+import { SelectCountryProps } from "./types";
+
+async function SelectCountry({
+  defaultCountry,
+  name,
+  id,
+  className,
+}: SelectCountryProps) {
+  const countries = await getCountries();
+
+  console.log(countries);
+  const flag =
+    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
+
+  return (
+    <select
+      name={name}
+      id={id}
+      defaultValue={`${defaultCountry}%${flag}`}
+      className={className}
+    >
+      <option value="">Select country...</option>
+      {countries.map((c) => (
+        <option key={c.name} value={`${c.name}%${c.flag}`}>
+          {c.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default SelectCountry;
