cf28fae63903c0b74b3b63a4e5630fdb7378ea00
Michael-Feleke <felekemichael999@gmail.com>
2024-10-18 12:17:23 +0300

    Add SignInButton component for displaying a button to sign in with Google

---
+++ app/_components/SignInButton.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+function SignInButton() {
+  return (
+    <button className="flex items-center gap-4 text-lg border border-primary-300 px-8 py-3 font-medium hover:bg-primary-900 hover:text-primary-100 transition-colors">
+      <Image
+        src="https://authjs.dev/img/providers/google.svg"
+        alt="Google logo"
+        height="24"
+        width="24"
+        className="h-6 w-6"
+      />
+      <span>Sign in with Google</span>
+    </button>
+  );
+}
+
+export default SignInButton;
