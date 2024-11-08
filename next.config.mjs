14f71a716e8ecbfdb4dcaa7c44592e2d820dc7e2
Michael-Feleke <felekemichael999@gmail.com>
2024-11-08 18:00:31 +0300

    configuration and adding packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // Replace with the Cloudinary domain
+        port: "",
+        pathname: "/**", // Allow all paths from Cloudinary
+      },
+    ],
+  },
+  // output:'export'
+};
 
 export default nextConfig;
