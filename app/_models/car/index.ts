2b1014deec34d08488309996e47975d17885ba70
Michael-Feleke <felekemichael999@gmail.com>
2024-11-11 23:17:11 +0300

    Add Car model definition with Mongoose schema and statics

---
+++ app/_models/car/index.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import * as Statics from "./statics";
+
+import { CarInterface, CarModelInterface } from "./types";
+import carSchema from "./schema";
+
+carSchema.static(Statics);
+
+const Car =
+  (mongoose.models.Car as unknown as CarModelInterface) ||
+  mongoose.model<CarInterface, CarModelInterface>("Car", carSchema);
+
+export default Car;
