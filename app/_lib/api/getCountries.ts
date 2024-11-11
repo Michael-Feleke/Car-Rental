e6ce615257b7657f0000ddecd1a34e2808b5fcd3
Michael-Feleke <felekemichael999@gmail.com>
2024-11-11 23:16:40 +0300

    Add getCountries function and Country type definition for country data fetching

---
+++ app/_lib/api/getCountries.ts
@@ -0,0 +1,27 @@
+import { errorMessages } from "@/app/_utils/messages/errorMessages";
+import logger from "@/app/_config/winston";
+import { Country } from "./types";
+import axios from "axios";
+import { COUNTRIES_API_URL } from "@/app/_utils/constants";
+
+export async function getCountries() {
+  try {
+    const response = await axios.get(COUNTRIES_API_URL);
+    const data: Country[] = response.data;
+
+    return data
+      .map((country) => ({
+        name: country.name.common,
+        flag: country.flags.png,
+      }))
+      .sort((a, b) => a.name.localeCompare(b.name));
+  } catch (error: unknown) {
+    if (error instanceof Error)
+      logger.error(errorMessages.countryFetchFailed(error.message));
+    else
+      logger.error(
+        errorMessages.countryFetchFailed(errorMessages.unknownError)
+      );
+    return [];
+  }
+}
