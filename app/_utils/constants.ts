18c2f682ad4a0fb1f83f5d27c5ecdd889c1db461
Michael-Feleke <felekemichael999@gmail.com>
2024-11-11 23:17:33 +0300

    Add route constants for booking and revalidation times; define API URL for countries

--- app/_utils/constants.ts
+++ app/_utils/constants.ts
@@ -1,10 +1,18 @@
-export const routeConstants = {
+export const ROUTE_CONSTANTS = {
   home: "/",
   cars: "/cars",
   about: "/about",
+  booking: "/booking",
   account: {
     base: "/account",
     profile: "/account/profile",
     reservations: "/account/reservations",
   },
 };
+
+// revalidate time in seconds
+export const CAR_LISTS_REVALIDATE_TIME = 3600;
+export const ABOUT_PAGE_REVALIDATE_TIME = 86400;
+
+// api URL
+export const COUNTRIES_API_URL = "https://restcountries.com/v3.1/all";
