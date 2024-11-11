cb8903bbfcba579b7fff8aff4cd0c6fef887d937
Michael-Feleke <felekemichael999@gmail.com>
2024-11-11 23:17:39 +0300

    Add enums for environments, route labels, meta titles, and Mongoose connection events

---
+++ app/_utils/enums.ts
@@ -0,0 +1,29 @@
+export enum Environments {
+  Development = "development",
+  Test = "test",
+  Production = "production",
+}
+
+export enum RouteLabels {
+  Home = "Home",
+  Cars = "Cars",
+  About = "About",
+  Account = "Your Account",
+  Profile = "Profile",
+  Reservations = "Reservations",
+}
+
+export enum MetaTitles {
+  Home = "Home",
+  Cars = "Cars",
+  About = "About",
+  Account = "Account",
+  Profile = "Update Profile",
+  Reservations = "Reservations",
+}
+
+export enum MongooseConnectionEvent {
+  Connected = "connected",
+  Disconnected = "disconnected",
+  Error = "error",
+}
