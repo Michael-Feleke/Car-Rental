12179d457ff0582a3ccf184b4fe49e64fd54a16a
Michael-Feleke <felekemichael999@gmail.com>
2024-11-12 22:05:25 +0300

    Update findAllCars function to filter cars by capacity

--- app/_models/car/statics.ts
+++ app/_models/car/statics.ts
@@ -1,8 +1,20 @@
 import { Model } from "mongoose";
 import { CarInterface } from "./types";
 
-export async function findAllCars(this: Model<CarInterface>) {
-  return await this.find({});
+export async function findAllCars(
+  this: Model<CarInterface>,
+  capacity: "large" | "medium" | "small"
+) {
+  let matchCondition = {};
+  if (capacity === "small") matchCondition = { maxCapacity: { $lte: 4 } };
+  else if (capacity === "medium")
+    matchCondition = { maxCapacity: { $gt: 4, $lte: 6 } };
+  else if (capacity === "large") matchCondition = { maxCapacity: { $gt: 6 } };
+  return await this.aggregate([
+    {
+      $match: matchCondition,
+    },
+  ]);
 }
 
 export async function findCarById(this: Model<CarInterface>, id: string) {
