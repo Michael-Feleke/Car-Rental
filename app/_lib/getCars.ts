072ce309270bc266cf9c42c8579ba01d37729e58
Michael-Feleke <felekemichael999@gmail.com>
2024-11-12 22:05:20 +0300

    Update getCars function to accept capacity parameter for fetching cars

--- app/_lib/getCars.ts
+++ app/_lib/getCars.ts
@@ -2,8 +2,10 @@ import Car from "../_models/car";
 import { CarInterface } from "../_models/car/types";
 import clientPromise from "./mongoose";
 
-export async function getCars(): Promise<CarInterface[]> {
+export async function getCars(
+  capacity: "large" | "small" | "medium"
+): Promise<CarInterface[]> {
   await clientPromise();
-  const cars = await Car.findAllCars();
+  const cars = await Car.findAllCars(capacity);
   return cars;
 }
