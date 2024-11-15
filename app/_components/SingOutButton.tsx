dda8741b3eb9fe6a7de08f1970558558eb41c416
Michael-Feleke <felekemichael999@gmail.com>
2024-11-16 00:04:11 +0300

    Wrap SignOutButton in a form to handle sign-out action

--- app/_components/SingOutButton.tsx
+++ app/_components/SingOutButton.tsx
@@ -1,11 +1,14 @@
 import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
+import { signOutAction } from "../_lib/actions";
 
 function SignOutButton() {
   return (
-    <button className="py-3 px-5 hover:bg-gray-800 text-gray-300  hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full">
-      <ArrowRightCircleIcon className="h-5 w-5 text-primary-600" />
-      <span>Sign out</span>
-    </button>
+    <form action={signOutAction}>
+      <button className="py-3 px-5 hover:bg-gray-800 text-gray-300  hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full">
+        <ArrowRightCircleIcon className="h-5 w-5 text-primary-600" />
+        <span>Sign out</span>
+      </button>
+    </form>
   );
 }
 
