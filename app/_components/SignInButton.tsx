b86b9d768783fce02a09a52250d7894b914122aa
Michael-Feleke <felekemichael999@gmail.com>
2024-11-16 00:04:05 +0300

    Wrap SignInButton in a form to handle sign-in action

--- app/_components/SignInButton.tsx
+++ app/_components/SignInButton.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
+import { signInAction } from "../_lib/actions";
 
 function SignInButton() {
   return (
-    <button className="flex items-center gap-4 text-lg border border-primary-300 px-8 py-3 font-medium hover:bg-primary-900 hover:text-primary-100 transition-colors">
-      <Image
-        src="https://authjs.dev/img/providers/google.svg"
-        alt="Google logo"
-        height="24"
-        width="24"
-        className="h-6 w-6"
-      />
-      <span>Sign in with Google</span>
-    </button>
+    <form action={signInAction}>
+      <button className="flex items-center gap-4 text-lg border border-primary-300 px-8 py-3 font-medium hover:bg-primary-900 hover:text-primary-100 transition-colors">
+        <Image
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+          className="h-6 w-6"
+        />
+        <span>Sign in with Google</span>
+      </button>
+    </form>
   );
 }
 
