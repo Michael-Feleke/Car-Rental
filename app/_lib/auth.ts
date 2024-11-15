2753053d3e8c2e03d303041d160c7d077581415e
Michael-Feleke <felekemichael999@gmail.com>
2024-11-16 00:04:27 +0300

    Add custom sign-in page route to authentication configuration

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,6 +1,7 @@
 import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } from "../_config/environments";
 import NextAuth, { Session } from "next-auth";
 import Google from "next-auth/providers/google";
+import { ROUTE_CONSTANTS } from "../_utils/constants";
 
 const authConfig = {
   providers: [
@@ -14,9 +15,18 @@ const authConfig = {
       return !!auth?.user;
     },
   },
+  pages: {
+    signIn: ROUTE_CONSTANTS.login,
+    // signOut: "/auth/signout",
+    // error: "/auth/error",
+    // verifyRequest: "/auth/verify-request",
+    // newUser: null,
+  },
 };
 
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
