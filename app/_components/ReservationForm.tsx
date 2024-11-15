4968f552b949a8b068db8413ddad53bd6b91d24d
Michael-Feleke <felekemichael999@gmail.com>
2024-11-15 21:31:48 +0300

    Enhance ReservationForm to display user information with profile image

--- app/_components/ReservationForm.tsx
+++ app/_components/ReservationForm.tsx
@@ -1,22 +1,26 @@
-function ReservationForm() {
+import Image from "next/image";
+import { ReservationFormProps } from "./types";
+
+function ReservationForm({ user }: ReservationFormProps) {
   // CHANGE
   const maxCapacity = 10;
 
   return (
     <div className="scale-[1.01]">
       <div className="bg-gray-800 px-16 py-2 flex justify-between items-center">
-        <p>Logged in as</p>
+        <p>Logged in as {user.name}</p>
 
-        {/* <div className='flex gap-4 items-center'>
-            <img
-              // Important to display google profile images
-              referrerPolicy='no-referrer'
-              className='h-8 rounded-full'
-              src={user.image}
-              alt={user.name}
-            />
-            <p>{user.name}</p>
-          </div> */}
+        <div className="flex gap-4 items-center">
+          <Image
+            width={40}
+            height={40}
+            referrerPolicy="no-referrer"
+            className="h-8 w-8 rounded-full"
+            src={user?.image || ""}
+            alt={user?.name || "image of the logged in user"}
+          />
+          <p>{user.name}</p>
+        </div>
       </div>
 
       <form className="bg-gray-900 py-10 px-16 text-lg flex gap-5 flex-col">
