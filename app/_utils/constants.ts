ebae5ad20e30092d2ebfb1dde26cb4cc77805f3c
Michael-Feleke <felekemichael999@gmail.com>
2024-11-18 00:50:25 +0300

    feat: integrate LogLevels enum and add color coding for log levels in constants

--- app/_utils/constants.ts
+++ app/_utils/constants.ts
@@ -1,3 +1,5 @@
+import { LogLevels } from "./enums";
+
 export const ROUTE_CONSTANTS = {
   home: "/",
   cars: "/cars",
@@ -11,11 +13,19 @@ export const ROUTE_CONSTANTS = {
 };
 
 export const LOG_LEVELS = {
-  debug: 0,
-  info: 1,
-  warn: 2,
-  error: 3,
+  [LogLevels.Debug]: 0,
+  [LogLevels.Info]: 1,
+  [LogLevels.Warn]: 2,
+  [LogLevels.Error]: 3,
 } as const;
+
+export const COLORS = {
+  [LogLevels.Info]: "\x1b[32m", // Green
+  [LogLevels.Warn]: "\x1b[33m", // Yellow
+  [LogLevels.Error]: "\x1b[31m", // Red
+  [LogLevels.Debug]: "\x1b[34m", // Blue
+  reset: "\x1b[0m", // Reset to default
+};
 
 export const ABOUT_PAGE_REVALIDATE_TIME = 86400;
 
