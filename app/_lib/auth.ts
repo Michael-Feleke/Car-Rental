65f59e31f5c3aba6d01219bbef054d53f52188fc
Michael-Feleke <felekemichael999@gmail.com>
2024-11-17 23:38:48 +0300

    feat: update Google authentication to use new environment variables and add user creation logic on sign-in

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,18 +1,47 @@
-import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } from "../_config/environments";
-import NextAuth, { Session } from "next-auth";
+import {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+} from "../_config/environments";
+import NextAuth, { Session, User } from "next-auth";
 import Google from "next-auth/providers/google";
 import { ROUTE_CONSTANTS } from "../_utils/constants";
+import { createUser, getUser } from "../_services/user";
+import { UserInterface } from "../_models/user/types";
+import { errorMessages } from "../_utils/messages/errorMessages";
 
 const authConfig = {
   providers: [
     Google({
-      clientId: AUTH_GOOGLE_ID,
-      clientSecret: AUTH_GOOGLE_SECRET,
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
     authorized({ auth }: { auth: Session | null }) {
       return !!auth?.user;
+    },
+    async signIn({ user }: { user: User }) {
+      try {
+        const email = user?.email ?? "";
+        const name = user?.name ?? "";
+
+        if (!email || !name) {
+          throw new Error(errorMessages.notValidUser);
+        }
+
+        const fetchedUser = await getUser(email);
+        if (!fetchedUser) {
+          await createUser({ email, name } as Omit<UserInterface, "_id">);
+          console.log("User created successfully");
+        } else {
+          console.log("User already exists");
+        }
+
+        return true;
+      } catch (error) {
+        console.error("Error during signIn callback:", error);
+        return false;
+      }
     },
   },
   pages: {
