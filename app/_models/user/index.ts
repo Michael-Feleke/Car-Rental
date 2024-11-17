9f9646b92631827201f07fd70c3b28883ace8067
Michael-Feleke <felekemichael999@gmail.com>
2024-11-18 00:39:40 +0300

    fix: update mongoose model access to ensure compatibility with existing models

--- app/_models/user/index.ts
+++ app/_models/user/index.ts
@@ -5,10 +5,9 @@ import { UserInterface, UserModelInterface } from "./types";
 import userSchema from "./schema";
 
 userSchema.static(Statics);
-console.log("Mongoose models:", mongoose.models);
 
 const User =
-  (mongoose?.models?.User as unknown as UserModelInterface) ||
+  (mongoose.models?.User as unknown as UserModelInterface) ||
   mongoose.model<UserInterface, UserModelInterface>("User", userSchema);
 
 export default User;
