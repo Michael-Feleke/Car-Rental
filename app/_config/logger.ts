2f9830e5d6ef200d4b74cd61ac4642536fd3f4a1
Michael-Feleke <felekemichael999@gmail.com>
2024-11-18 09:47:56 +0300

    feat: update logger to accept unknown message types and format objects

--- app/_config/logger.ts
+++ app/_config/logger.ts
@@ -6,16 +6,22 @@ type LogLevel = keyof typeof LOG_LEVELS;
 
 const currentLogLevel: LogLevel = LOG_LEVEL as LogLevel;
 
-const simpleLogger = (level: LogLevel, message: string): void => {
+const simpleLogger = (level: LogLevel, message: unknown): void => {
   if (ENVIRONMENT === Environments.Test) return;
 
   if (LOG_LEVELS[level] >= LOG_LEVELS[currentLogLevel]) {
     const timestamp = new Date().toISOString();
     const color = COLORS[level] || COLORS.reset;
+
+    const formattedMessage =
+      typeof message === "object" && message !== null
+        ? JSON.stringify(message, null, 2)
+        : message;
+
     const logMessage =
       ENVIRONMENT === Environments.Production
-        ? `${timestamp} [${color}${level}${COLORS.reset}] ${message}`
-        : `[${color}${level}${COLORS.reset}] ${message}`;
+        ? `${timestamp} [${color}${level}${COLORS.reset}] ${formattedMessage}`
+        : `[${color}${level}${COLORS.reset}] ${formattedMessage}`;
 
     if (level === LogLevels.Error) {
       console.error(logMessage);
@@ -28,10 +34,10 @@ const simpleLogger = (level: LogLevel, message: string): void => {
 };
 
 const logger = {
-  info: (message: string) => simpleLogger(LogLevels.Info, message),
-  warn: (message: string) => simpleLogger(LogLevels.Warn, message),
-  error: (message: string) => simpleLogger(LogLevels.Error, message),
-  debug: (message: string) => simpleLogger(LogLevels.Debug, message),
+  info: (message: unknown) => simpleLogger(LogLevels.Info, message),
+  warn: (message: unknown) => simpleLogger(LogLevels.Warn, message),
+  error: (message: unknown) => simpleLogger(LogLevels.Error, message),
+  debug: (message: unknown) => simpleLogger(LogLevels.Debug, message),
 };
 
 export default logger;
