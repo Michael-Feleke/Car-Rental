7c8014217fbfde638e48920faa68f85e94b474cf
Michael-Feleke <felekemichael999@gmail.com>
2024-11-21 00:58:25 +0300

    feat: add handleDeleteReservation function to manage reservation deletions

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -7,6 +7,7 @@ import { ROUTE_CONSTANTS } from "../_utils/constants";
 import { withMiddleware } from "../_utils/helpers/withMiddleware";
 import { errorMessages } from "../_utils/messages/errorMessages";
 import { signIn, signOut } from "./auth";
+import { deleteReservation } from "../_services/reservation";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: ROUTE_CONSTANTS.account.base });
@@ -20,7 +21,7 @@ export const updateProfile = withMiddleware(async (session, formData) => {
   const userId = session.user?.userId;
   if (!userId) throw new Error(errorMessages.authorizationError);
 
-  const countryValue = formData.get("country");
+  const countryValue = formData?.get("country");
   let country = "";
   let countryFlag = "";
 
@@ -28,7 +29,7 @@ export const updateProfile = withMiddleware(async (session, formData) => {
     [country, countryFlag] = countryValue.split("%");
   }
 
-  const drivingLicense = formData.get("drivingLicense") as string;
+  const drivingLicense = formData?.get("drivingLicense") as string;
 
   await updateUser(userId, {
     country,
@@ -40,3 +41,9 @@ export const updateProfile = withMiddleware(async (session, formData) => {
     `${ROUTE_CONSTANTS.account.base}/${ROUTE_CONSTANTS.account.profile}`
   );
 }, updateProfileSchema);
+
+export const handleDeleteReservation = async (reservationId: string) => {
+  await deleteReservation(reservationId);
+
+  revalidatePath(`${ROUTE_CONSTANTS.account.reservations}`);
+};
