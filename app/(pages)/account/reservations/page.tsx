17d0abec8b7477bdeef16031f6fbd59d2cc88d3b
Michael-Feleke <felekemichael999@gmail.com>
2024-11-20 23:33:12 +0300

    feat: implement reservations retrieval and display in account page

--- app/(pages)/account/reservations/page.tsx
+++ app/(pages)/account/reservations/page.tsx
@@ -1,15 +1,17 @@
-import { ROUTE_CONSTANTS } from "@/app/_utils/constants";
+import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getReservations } from "@/app/_services/reservation";
 import { MetaTitles } from "@/app/_utils/enums";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: MetaTitles.Reservations,
 };
 
-export default function Page() {
-  // CHANGE
-  // const bookings: any = [];
+export default async function Page() {
+  const session = await auth();
+  const userId = session?.user?.userId || "";
+  const reservations = await getReservations(userId);
 
   return (
     <div>
@@ -17,28 +19,11 @@ export default function Page() {
         Your car reservations
       </h2>
 
-      {
-        true ? (
-          <p className="text-lg">
-            You have no reservations yet. Check out our{" "}
-            <Link
-              className="underline text-red-500"
-              href={ROUTE_CONSTANTS.cars}
-            >
-              available cars &rarr;
-            </Link>
-          </p>
-        ) : (
-          ""
-        )
-        //  : (
-        //   <ul className="space-y-6">
-        //     {bookings.map((booking) => (
-        //       <ReservationCard booking={booking} key={booking.id} />
-        //     ))}
-        //   </ul>
-        // )
-      }
+      <ul className="space-y-6">
+        {reservations.map((reservation) => (
+          <ReservationCard reservation={reservation} key={reservation._id} />
+        ))}
+      </ul>
     </div>
   );
 }
