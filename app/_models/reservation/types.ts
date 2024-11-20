e86896b65bd88bd3e3f068af60cd75d8df9bd28b
Michael-Feleke <felekemichael999@gmail.com>
2024-11-20 23:33:49 +0300

    feat: update Reservation types to include IPopulatedReservation and enhance getReservations method

--- app/_models/reservation/types.ts
+++ app/_models/reservation/types.ts
@@ -1,4 +1,6 @@
 import { Document, Model } from "mongoose";
+import { UserInterface } from "../user/types";
+import { CarInterface } from "../car/types";
 
 export interface ReservationInterface extends Document {
   _id: string;
@@ -11,9 +13,21 @@ export interface ReservationInterface extends Document {
   totalPrice: number;
 }
 
+export interface IPopulatedReservation {
+  _id: string;
+  user: Partial<UserInterface>;
+  car: Partial<CarInterface>;
+  startDate: Date;
+  endDate: Date;
+  status: string;
+  numberOfDays: number;
+  totalPrice: number;
+  createdAt: Date;
+}
+
 export interface ReservationModelInterface extends Model<ReservationInterface> {
   createReservation(
     reservation: Omit<ReservationInterface, "_id">
   ): Promise<ReservationInterface>;
-  getReservations(id: string): Promise<ReservationInterface>;
+  getReservations(id: string): Promise<IPopulatedReservation[]>;
 }
