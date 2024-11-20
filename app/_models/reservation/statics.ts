a38ddd55c58e5d070b6377635b2e7119079b4819
Michael-Feleke <felekemichael999@gmail.com>
2024-11-20 20:07:05 +0300

    feat: add static methods for creating and retrieving reservations

---
+++ app/_models/reservation/statics.ts
@@ -0,0 +1,18 @@
+import { Model } from "mongoose";
+import { ReservationInterface } from "./types";
+
+export async function createReservation(
+  this: Model<ReservationInterface>,
+  reservation: ReservationInterface
+) {
+  return this.create(reservation);
+}
+
+export async function getReservations(
+  this: Model<ReservationInterface>,
+  id: string
+) {
+  return this.find({ user: id })
+    .populate("user", "name email")
+    .populate("car", "name image");
+}
