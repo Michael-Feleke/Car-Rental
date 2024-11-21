153cd1eb886c1b796d8e118927f46a03c63c54d8
Michael-Feleke <felekemichael999@gmail.com>
2024-11-21 11:35:58 +0300

    chore: comment out reservation creation logic in account page

--- app/(pages)/account/page.tsx
+++ app/(pages)/account/page.tsx
@@ -8,6 +8,20 @@ export const metadata: Metadata = {
 };
 
 async function page() {
+  // To create a new reservation
+
+  // const reservation = {
+  //   user: "673a650b7901aaf66dc016d7",
+  //   car: "672e484bd648b472f1c1c18c",
+  //   startDate: new Date("2024-11-21T10:00:00Z"), // Example start date
+  //   endDate: new Date("2024-11-25T10:00:00Z"), // Example end date
+  //   status: "confirmed", // Example status
+  //   numberOfDays: 4, // Calculated based on the example dates
+  //   numberOfPassengers: 5,
+  //   totalPrice: 400, // Example total price
+  // };
+
+  // await createReservation(reservation);
   const session = await getSession();
   return (
     <h2 className="text-2xl  text-red-500">Welcome, {session?.user?.name}</h2>
@@ -15,20 +29,6 @@ async function page() {
 }
 
 export default page;
-
-// To create a new reservation
-
-// const reservation = {
-//   user: "673a650b7901aaf66dc016d7",
-//   car: "672e484bd648b472f1c1c18c",
-//   startDate: new Date("2024-11-21T10:00:00Z"), // Example start date
-//   endDate: new Date("2024-11-25T10:00:00Z"), // Example end date
-//   status: "confirmed", // Example status
-//   numberOfDays: 4, // Calculated based on the example dates
-//   totalPrice: 400, // Example total price
-// };
-
-// await createReservation(reservation);
 
 //car id 672e484bd648b472f1c1c18c
 //car id 2 673389602055984574c1c18c
