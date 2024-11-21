dbf3414cf1ff17987f61bc97433457c6ac7b045d
Michael-Feleke <felekemichael999@gmail.com>
2024-11-21 18:45:07 +0300

    feat: add editReservationSchema for validating reservation updates

--- app/_schema/validationSchema.ts
+++ app/_schema/validationSchema.ts
@@ -22,3 +22,23 @@ export const updateProfileSchema = Joi.object({
     "any.required": `"drivingLicense" is a required field`,
   }),
 });
+
+export const editReservationSchema = Joi.object({
+  reservationId: Joi.string().required().messages({
+    "string.base": `"reservationId" should be a type of 'text'`,
+    "string.empty": `"reservationId" cannot be empty`,
+    "any.required": `"reservationId" is a required field`,
+  }),
+
+  numberOfPassengers: Joi.number().required().messages({
+    "number.base": `"numberOfPassengers" should be a type of 'number'`,
+    "number.empty": `"numberOfPassengers" cannot be empty`,
+    "any.required": `"numberOfPassengers" is a required field`,
+  }),
+
+  description: Joi.string().min(0).max(500).messages({
+    "string.base": `"description" should be a type of 'text'`,
+    "string.min": `"description" should have a minimum length of 10 characters`,
+    "string.max": `"description" should have a maximum length of 500 characters`,
+  }),
+});
