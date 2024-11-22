9a6466ea3206b3f2204988866adf2efad9cc80d6
Michael-Feleke <felekemichael999@gmail.com>
2024-11-22 10:50:48 +0300

    feat: update Reservations component to include car data and reserved date ranges

--- app/_components/Reservations.tsx
+++ app/_components/Reservations.tsx
@@ -1,16 +1,34 @@
+import { getCarReservationByAllUsers } from "../_services/reservation";
 import { getSetting } from "../_services/setting";
 import { getSession } from "../_utils/helpers/getSession";
 import DateSelector from "./DateSelector";
 import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
+import { ReservationsProps } from "./types";
 
-async function Reservations() {
-  const setting = await getSetting();
+async function Reservations({ car }: ReservationsProps) {
   const session = await getSession();
+  const setting = await getSetting();
+  const reservations = await getCarReservationByAllUsers(car._id.toString());
+
+  const reservedRanges = reservations.map((reservation) => ({
+    from: new Date(reservation.startDate),
+    to: new Date(reservation.endDate),
+  }));
+
+  const { dailyPrice, discount } = car;
+  const carData = { dailyPrice, discount };
+
+  const { minRentDuration, maxRentDuration } = setting;
+  const plainSetting = { minRentDuration, maxRentDuration };
 
   return (
     <>
-      <DateSelector setting={setting} />
+      <DateSelector
+        setting={plainSetting}
+        car={carData}
+        reservations={reservedRanges}
+      />
       {session?.user ? (
         <ReservationForm user={session.user} />
       ) : (
