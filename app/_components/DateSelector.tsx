1a82efa56a41a28b2abd064c8334684f47d972a4
Michael-Feleke <felekemichael999@gmail.com>
2024-11-22 10:50:42 +0300

    feat: enhance DateSelector component to calculate car rental price and disable reserved dates

--- app/_components/DateSelector.tsx
+++ app/_components/DateSelector.tsx
@@ -1,31 +1,41 @@
 "use client";
 
-// import { isWithinInterval } from "date-fns";
-import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-import { DateSelectorProps } from "./types";
+
+import { isWithinInterval } from "date-fns";
+import { DayPicker } from "react-day-picker";
 import { useReservation } from "./ReservationContext";
+import { differenceInDays } from "date-fns";
+import { DateSelectorProps } from "./types";
 
-// function isAlreadyBooked(range: DateRange, datesArr: Date[]) {
-//   return (
-//     range.from &&
-//     range.to &&
-//     datesArr.some((date) =>
-//       isWithinInterval(date, { start: range.from, end: range.to })
-//     )
-//   );
-// }
-
-function DateSelector({ setting }: DateSelectorProps) {
+function DateSelector({ setting, car, reservations }: DateSelectorProps) {
   const { minRentDuration, maxRentDuration } = setting;
-
   const { range, setRange, resetRange } = useReservation();
+  const { dailyPrice, discount } = car;
 
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numberOfDays = 23;
-  const carPrice = 23;
+  const numberOfDays =
+    range?.from && range?.to ? differenceInDays(range.to, range.from) : 0;
+  const carPrice = numberOfDays * (dailyPrice - discount);
+
+  const isDateReserved = (date: Date) => {
+    return reservations.some((reservedRange) => {
+      return (
+        reservedRange.from &&
+        reservedRange.to &&
+        isWithinInterval(date, {
+          start: reservedRange.from,
+          end: reservedRange.to,
+        })
+      );
+    });
+  };
+
+  const disabledDays = [
+    {
+      before: new Date(),
+    },
+    (date: Date) => isDateReserved(date),
+  ];
 
   return (
     <div className="flex flex-col justify-between border border-gray-700">
@@ -45,6 +55,7 @@ function DateSelector({ setting }: DateSelectorProps) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={1}
+        disabled={disabledDays}
       />
 
       <div className="flex items-center justify-between px-8 bg-gray-600  h-[72px]">
@@ -52,23 +63,23 @@ function DateSelector({ setting }: DateSelectorProps) {
           <p className="flex gap-2 items-baseline">
             {discount > 0 ? (
               <>
-                <span className="text-2xl">${regularPrice - discount}</span>
+                <span className="text-2xl">${dailyPrice - discount}</span>
                 <span className="line-through font-semibold">
-                  ${regularPrice}
+                  ${dailyPrice}
                 </span>
               </>
             ) : (
-              <span className="text-2xl">${regularPrice}</span>
+              <span className="text-2xl">${dailyPrice}</span>
             )}
             <span className="">/night</span>
           </p>
-          {numberOfDays ? (
+          {numberOfDays > 0 ? (
             <>
               <p className=" px-3 py-2 bg-gray-800 text-2xl">
                 <span>&times;</span> <span>{numberOfDays}</span>
               </p>
-              <p>
-                <span className="text-lg font-bold uppercase">Total</span>{" "}
+              <p className="mr-2">
+                <span className="text-lg font-bold uppercase">Total=</span>{" "}
                 <span className="text-2xl font-semibold">${carPrice}</span>
               </p>
             </>
@@ -80,7 +91,7 @@ function DateSelector({ setting }: DateSelectorProps) {
             className="border border-red-800 py-2 px-4 text-sm font-semibold"
             onClick={() => resetRange()}
           >
-            Clear,
+            Clear
           </button>
         ) : null}
       </div>
